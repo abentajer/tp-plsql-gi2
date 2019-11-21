3e1c29bac927723b304e191f0e06efdd623e431d
Chaimae-27 <chaimae.elkhnissi@gmail.com>
2019-11-21 18:46:39 +0000

    Remplissage des tables

---
+++ insertion_tables.sql
@@ -0,0 +1,27 @@
+
+
+/**************** Remplissage des tables*****************/
+
+INSERT INTO Filiere('1','Informatique');
+INSERT INTO Filiere('2','Logistique');
+INSERT INTO Filiere('3','Civil');
+INSERT INTO Filiere('4','Mécatronique');
+INSERT INTO Filiere('5','Réseaux');
+
+
+
+
+INSERT INTO Electeurs('1','alaoui','Ayman','12/05/1996','12','20','1','1');
+INSERT INTO Electeurs('2','chamali','khadija','10/07/1996','15','22','0','2');
+INSERT INTO Electeurs('3','halfaoui','Oumnia','01/02/1996','1','09','1','5');
+INSERT INTO Electeurs('4','bennani','Ahmed','19/03/1996','21','10','1','3');
+INSERT INTO Electeurs('5','ahabrach','Wael','22/08/1996','5','0','1','4');
+INSERT INTO Electeurs('6','harak','Ayoub','15/10/1996','10','2','0','4');
+INSERT INTO Electeurs('7','moussaoui','KHalid','05/09/1996','19','20','1','2');
+INSERT INTO Electeurs('8','hadri','Samira','17/12/1996','15','20','0','5');
+INSERT INTO Electeurs('9','boukhari','Akram','23/02/1996','20','21','0','3');
+INSERT INTO Electeurs('10','zanati','Idriss','30/03/1996','1','32','0','1');
+
+
+
+
