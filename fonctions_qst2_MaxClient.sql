5e7c4280f836c8ad04e953856fd870bc5861ac3a
fitian98 <chaimaefitian@gmail.com>
2019-11-24 22:03:56 +0100

    question 2-1 de la partis fonctions

---
+++ fonctions_qst2_MaxClient.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION MaxClient
+RETURN varchar2 IS 
+   total varchar2(100);  
+BEGIN 
+   
+    SELECT c INTO  total FROM ( SELECT SUM(commande.montant_ttc) AS qte , client.nom AS c FROM commande , client
+    WHERE commande.code_commande = client.code_commande GROUP BY client.nom)ORDER BY qte desc FETCH NEXT 1 ROWS ONLY;
+    RETURN total; 
+END;
+
+/*exemple d'execution*/
+set serveroutput on;
+DECLARE
+ c VARCHAR2(100);
+BEGIN 
+    
+   c := MaxClient(); 
+   dbms_output.put_line('le client qui a passé le max des achats est Mr ' || c); 
+END;
