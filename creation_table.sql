7a02ffa139e1e2425bcb018e60d4a1f81dd0fa6b
aya-yas <ayaenissay1997@gmail.com>
2019-11-24 22:44:22 +0100

    creation des tables

---
+++ creation_table.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE filiere(
+idfil NUMBER(5) PRIMARY KEY,
+nomfil VARCHAR2(50) NOT NULL
+);
+
+
+CREATE TABLE electeurs(
+idelec NUMBER(5) PRIMARY KEY,
+nom VARCHAR2(20) NOT NULL,
+pnom VARCHAR2(20),
+ddn DATE ,
+nvote NUMBER(5),
+nvoix NUMBER(5),
+cand NUMBER(5),
+idfl NUMBER(5),
+CONSTRAINT idfl
+    FOREIGN KEY (idfl)
+    REFERENCES filiere(idfil)
+);
