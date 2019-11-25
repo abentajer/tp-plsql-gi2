0087e7ab5403b29457d46faafc0472569cd63c22
aya-yas <ayaenissay1997@gmail.com>
2019-11-25 14:22:35 +0100

    question 8 curseur

---
+++ affichage_candidat_voix.sql
@@ -0,0 +1,19 @@
+/*un curseur qui afficher la liste des candidats ayant nvoix > 10 puis ajouter leurs noms dans une nested-table*/
+
+DECLARE
+	Cursor ListCandidatVoix is
+		SELECT nom,pnom FROM Electeurs
+		WHERE cand=1 and nvoix>10;
+	TYPE n_list IS TABLE of Electeurs.nom%type;
+	TYPE p_list IS TABLE of Electeurs.pnom%type;
+	nom_list n_list;
+	pnom_list p_list;
+	counter integer:=0; 
+BEGIN
+	FOR n IN ListCandidatVoix LOOP
+		counter:=counter+1;
+		nom_list(counter):=n.nom;
+		pnom_list(counter):=n.pnom;
+		dbms_output.put_line(nom_list(counter)||""||pnom_list(counter));
+	END LOOP;
+END;
