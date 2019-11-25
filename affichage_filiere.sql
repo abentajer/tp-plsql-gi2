4d4acd782d0a6d41c7dfed88458ba3bac632a9cd
aya-yas <ayaenissay1997@gmail.com>
2019-11-25 14:00:59 +0100

    question 4 curseur

---
+++ affichage_filiere.sql
@@ -0,0 +1,16 @@
+/*Un curseur qui affiche la liste des filieres*/
+
+DECLARE
+	f_id Filiere.idfil%type;
+	f_nom Filiere.nomfil%type;
+	CURSOR ListeFiliere is
+		SELECT idfil,nomfil FROM Filiere;
+BEGIN
+	OPEN ListeFiliere;
+	LOOP
+	FETCH ListeFiliere into f_id,f_nom;
+		EXIT WHEN ListeFiliere%notfound;
+		dbms_output.put_line(f_id||''||f_nom);
+	END LOOP;
+	CLOSE ListeFiliere;
+END;
