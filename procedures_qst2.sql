4f4c793a155d7e9f85234f2730b245675e6b0a76
fitian98 <57631215+fitian98@users.noreply.github.com>
2019-11-26 13:15:03 +0100

    Update procedures_qst2.sql

--- procedures_qst2.sql
+++ procedures_qst2.sql
@@ -1,18 +1,14 @@
-/*une procédure pour inscrire les électeurs*/
+/*une procÃ©dure pour inscrire les Ã©lecteurs*/
 CREATE OR REPLACE PROCEDURE inscrire(
-	   c_id IN  tab_electeur.IDELEC%TYPE,
+	   c_id IN  tab_electeur.IDELEC%type,
 	   c_nom IN  tab_electeur.NOM%type,
 	   c_pnom IN  tab_electeur.PNOM%type,
-	   c_ddn IN   tab_electeur.DDN%type,
-	   c_nvot IN  tab_electeur.NVOT%type,
-	   c_nvoix IN  tab_electeur.NVOIX%type,
-	   c_cand IN   tab_electeur.CAND%type,
-	   c_idfl IN  tab_electeur.IDFL%type
+       	   c_ddn IN tab_electeur.DDN%type
 )
 IS
 BEGIN
 
  	INSERT INTO tab_electeur ("IDELEC", "NOM", "PNOM", "DDN") 
-  VALUES (c_idelec,c_nom,c_pnom, c_ddn);
+  	VALUES (c_id,c_nom,c_pnom,c_ddn);
 
 END;
