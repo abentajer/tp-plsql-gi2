10965cb365ca7fa1d4f1ae28b047fe8ad9073a71
fitian98 <57631215+fitian98@users.noreply.github.com>
2019-11-26 19:20:54 +0100

    Update procedures_qst3.sql

--- procedures_qst3.sql
+++ procedures_qst3.sql
@@ -1,17 +1,17 @@
-CREATE OR REPLACE PROCEDURE InsertCommande(
+CREATE OR REPLACE PROCEDURE Remise(
 	   p_Ncommande IN commande.CODE_COMMANDE%TYPE,
 	   p_Datecommande IN commande.DATE_COMMANDE%TYPE,
 	   p_Reglage IN commande.REGLAGE%TYPE,
 	   p_MontantHT IN commande.MONTANT_HT%TYPE,
-	   p_MontantTTC IN commande.MONTANT_TTC%TYPE,
-	   taux number:=(5,2))
-IS
+	   p_MontantTTC IN commande.MONTANT_TTC%TYPE
+)IS
 
-BEGIN
-    a:=p_Ncommande;  
-  INSERT INTO commande ("CODE_COMMANDE", "DATE_COMMANDE", "REGLAGE", "MONTANT_HT", "MONTANT_TTC") 
-  VALUES (a*(1-taux),p_Datecommande,p_Reglage,p_MontantHT, p_MontantTTC);
+    taux NUMBER(5,2);
+    a NUMBER := p_Ncommande; 
+    BEGIN
+ 
+         INSERT INTO commande ("CODE_COMMANDE", "DATE_COMMANDE", "REGLAGE", "MONTANT_HT", "MONTANT_TTC") 
+         VALUES (a*(1-taux),p_Datecommande,p_Reglage,p_MontantHT, p_MontantTTC);
+         COMMIT;
 
-  COMMIT;
-
-END;
+END Remise;
