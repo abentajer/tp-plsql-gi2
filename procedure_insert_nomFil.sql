538a9231cd92006fb6c33602d5dfa4be51505c16
aya-yas <ayaenissay1997@gmail.com>
2019-11-26 22:25:57 +0100

    question 10 procedure(updated)

--- procedure_insert_nomFil.sql
+++ procedure_insert_nomFil.sql
@@ -1,11 +1,11 @@
 /*une procédure pour ajouter les filières*/
 
 CREATE OR REPLACE PROCEDURE insertNameFILIERE(
-	   f_id IN Filiere.idfil%type,
-	   f_nomfil IN Filiere.nomfil%type
+	   f_id IN Filiere.IDFIL%type,
+	   f_nomfil IN Filiere.NOMFIL%type
 )
 IS
 BEGIN
-	INSERT INTO Filiere ("idfil","nomfil")
+	INSERT INTO tab_filiere ("IDFIL","NOMFIL")
 	VALUES (f_id,f_nomfil);
 END;
