b3ee5782f2c60e41f915c19b1156de051624c820
aya-yas <ayaenissay1997@gmail.com>
2019-11-26 22:39:31 +0100

    question 12 procedure(updated)

--- procedure_electeur_change_candidat.sql
+++ procedure_electeur_change_candidat.sql
@@ -1,10 +1,10 @@
 /*une procédure pour permettre à des électeurs de devenir candidat*/
 
 CREATE OR REPLACE PROCEDURE candidat(
-	  e_cand IN Electeurs.cand%type
+	  e_cand IN Electeurs.CAND%type
 )
 IS
 BEGIN 
 
-	UPDATE Electeurs SET e_cand = 1;
+	UPDATE Electeurs SET CAND = '1';
 END;
