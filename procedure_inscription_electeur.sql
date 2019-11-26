04eaa3a64b503c5233c20164d41e6058d5b1ed4f
aya-yas <ayaenissay1997@gmail.com>
2019-11-26 22:34:59 +0100

    question 11 procedure(updated)

--- procedure_inscription_electeur.sql
+++ procedure_inscription_electeur.sql
@@ -1,19 +1,19 @@
 /*une procédure pour inscrire les électeurs*/
 
 CREATE OR REPLACE PROCEDURE inscrire(
-	   e_id IN Electeurs.idelec%TYPE,
-	   e_nom IN Electeurs.nom%type,
-	   e_pnom IN Electeurs.pnom%type,
-	   e_ddn IN Electeurs.ddn%type,
-	   e_nvot IN Electeurs.nvot%type,
-	   e_nvoix IN Electeurs.nvoix%type,
-	   e_cand IN Electeurs.cand%type,
-	   e_idfl IN Electeurs.idfl%type
+	   e_idelec IN  Electeurs.IDELEC%type,
+	   e_nom IN  Electeurs.NOM%type,
+	   e_pnom IN  Electeurs.PNOM%type,
+       	   e_ddn IN Electeurs.DDN%type,
+	   e_nvot IN Electeurs.NVOT%type,
+	   e_nvoix IN Electeurs.NVOIX%type,
+	   e_cand IN Electeurs.CAND%type,
+	   e_idfl IN Electeurs.IDFL%type
 )
 IS
 BEGIN
 
- 	INSERT INTO Electeurs ("idelec", "nom", "pnom", "ddn") 
-  VALUES (e_idelec,e_nom,e_pnom, e_ddn);
+ 	INSERT INTO Electeurs ("IDELEC", "NOM", "PNOM", "DDN", "NVOT", "NVOIX", "CAND", "IDFL") 
+  	VALUES (e_idelec,e_nom,e_pnom,e_ddn,e_nvot,e_nvoix,e_cand,e_idfl);
 
 END;
