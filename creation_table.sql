3ea7212a2339b6cd42042ab1edb8af5b75f1bea7
aya-yas <ayaenissay1997@gmail.com>
2019-11-26 22:18:17 +0100

    creation des tables(updated)

--- creation_table.sql
+++ creation_table.sql
@@ -1,16 +1,16 @@
 
-CREATE TABLE filiere(
+CREATE TABLE Filiere(
 idfil NUMBER(5) PRIMARY KEY,
 nomfil VARCHAR2(50) NOT NULL
 );
 
 
-CREATE TABLE electeurs(
+CREATE TABLE Electeurs(
 idelec NUMBER(5) PRIMARY KEY,
 nom VARCHAR2(20) NOT NULL,
 pnom VARCHAR2(20),
 ddn DATE ,
-nvote NUMBER(5),
+nvot NUMBER(5),
 nvoix NUMBER(5),
 cand NUMBER(5),
 idfl NUMBER(5),
