5313e7a0c24c318fddb4ed8c0dca4ad3fa7e88d6
Chaimae-27 <chaimae.elkhnissi@gmail.com>
2019-11-29 21:20:36 +0000

    TP1 modelisation remplissage des tables **Updates**

--- remplissage_des_tables.sql
+++ remplissage_des_tables.sql
@@ -1,39 +1,46 @@
 /*on a remplit les tables selon les exemples donnés dans l'enonce de tp*/
 /*TABLE commande*/
-INSERT INTO commande VALUES ('886','01/02/2019','oui','4329,00','5036,40');
-INSERT INTO commande VALUES ('887','02/02/2019','oui','4650,00','5026,40');
-INSERT INTO commande VALUES ('888','03/02/2019','oui','3256,00','5224,50');
+INSERT INTO COMMANDE VALUES ('886','01/02/2019','oui','4329,00','5036,40','2');
+INSERT INTO COMMANDE VALUES ('887','02/02/2019','oui','4650,00','5026,40','1');
+INSERT INTO COMMANDE VALUES ('888','03/02/2019','oui','3256,00','5224,50','3');
 
 /*TABLE client*/
-INSERT INTO client VALUES('1', 'BENTAJER', 'Mhanech II Tetouan','ahmed@mail.com','2/11/2019','885');
-INSERT INTO client VALUES('2', 'ESSEGHIRI', 'Res Naoufal app 8,Maarif, Casablanca','youssef@mail.com','27/10/2018','886');
-INSERT INTO client VALUES('3', 'HOUTY','Settat','karim@mail.com','20/11/2018','887');
+INSERT INTO CLIENT VALUES('1', 'BENTAJER', 'Mhanech II Tetouan','ahmed@mail.com','2/11/2019','1');
+INSERT INTO CLIENT VALUES('2', 'ESSEGHIRI', 'Res Naoufal app 8,Maarif, Casablanca','youssef@mail.com','27/10/2018','2');
+INSERT INTO CLIENT VALUES('3', 'HOUTY','Settat','karim@mail.com','20/11/2018','2');
 
 /*TABLE region*/
-INSERT INTO region VALUES('1','Tanger-Tétouan-Al Hoceïma','1');
-INSERT INTO region VALUES('2','Région d_Oriental','2');
-INSERT INTO region VALUES('3','Région de Fès-Meknès','3');
+INSERT INTO REGION VALUES('1','Tanger-Tétouan-Al Hoceïma');
+INSERT INTO REGION VALUES('2','Région d_Oriental');
+INSERT INTO REGION VALUES('3','Région de Fès-Meknès');
+INSERT INTO REGION VALUES(4,'Région Rabat-Salé-Kénitra');
+INSERT INTO REGION VALUES(5,'Région Bni Mellal-Khénifra');
+INSERT INTO REGION VALUES(6,'Région de Casablanca-Settat');
+INSERT INTO REGION VALUES(7,'Région de Marrakech-Safi');
 
 /*TABLE produit*/
-INSERT INTO produit VALUES ('A01','Barrette Memoire 2 Go','150','0,12');
-INSERT INTO produit VALUES ('A02','Barrette Memoire 4Go','240','0,12');
-INSERT INTO produit VALUES ('B15','Carte Graphique AMD Radeon RX 570 Pulse, 4Go','2349','0,2');
-INSERT INTO produit VALUES ('A11','Carte Son Asus Xonar DG','750','0,12');
-INSERT INTO produit VALUES ('C80','Disque Externe Toshiba usb 3.0 1000Go','546','0,33');
+INSERT INTO PRODUIT VALUES ('A01','Barrette Memoire 2 Go',150,'12%',1,1);
+INSERT INTO PRODUIT VALUES ('A02','Barrette Memoire 4Go','240','12%',1,1);
+INSERT INTO PRODUIT VALUES ('B15','Carte Graphique AMD Radeon RX 570 Pulse, 4Go','2349','20%',2,5);
+INSERT INTO PRODUIT VALUES ('A11','Carte Son Asus Xonar DG','750','12%',2,4);
+INSERT INTO PRODUIT VALUES ('C80','Disque Externe Toshiba usb 3.0 1000Go','546','33%',3,2);
 
 /*TABLE categorie*/
-INSERT INTO categorie VALUES ('1','RAM','A01');
-INSERT INTO categorie VALUES ('2','DISQUE DUR EXTERNE','C80');
-INSERT INTO categorie VALUES ('4','CARTE SON','A11');
-INSERT INTO categorie VALUES ('5','CARTE GRAPHIQUE','B15');
+INSERT INTO CATEGORIE VALUES ('1','RAM');
+INSERT INTO CATEGORIE VALUES ('2','DISQUE DUR EXTERNE');
+INSERT INTO CATEGORIE VALUES ('3','DISQUE DUR INTERNE');
+INSERT INTO CATEGORIE VALUES ('4','CARTE SON');
+INSERT INTO CATEGORIE VALUES ('5','CARTE GRAPHIQUE');
 
-/*TABLE concerne*/
-INSERT INTO concerne VALUES ('10','886','A01');
-INSERT INTO concerne VALUES ('2','886','A02');
-INSERT INTO concerne VALUES ('1','886','B15');
+/*TABLE contenir*/
+INSERT INTO CONTENIR VALUES (10,'A01','886');
+INSERT INTO CONTENIR VALUES (2,'A02','886');
+INSERT INTO CONTENIR VALUES (1,'B15','886');
+INSERT INTO CONTENIR VALUES (20,'A01','123');
 
-/*TABLE catalogue*/
-INSERT INTO catalogue VALUES ('1','2','A01');
-INSERT INTO catalogue VALUES ('2','2','A02');
-INSERT INTO catalogue VALUES ('3','2','B15');
+/*Table famille*/
+
+INSERT INTO FAMILLE VALUES(1,'MEMOIRE');
+INSERT INTO FAMILLE VALUES(2,'CARTE');
+INSERT INTO FAMILLE VALUES(3,'DISQUE');
 
