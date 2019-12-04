4c13ba0d15e248a5bf080363fa63260ebebd1acc
fitian98 <chaimaefitian@gmail.com>
2019-12-04 22:34:55 +0100

    UPDATING QST 1 CURSEURS

--- curseurs_qst1.sql
+++ curseurs_qst1.sql
@@ -1,15 +1,17 @@
 /*Curseur permettant d'afficher les infos d'un produit*/
 DECLARE
-	c_Ref Produit.REF%type;
-	c_Libelle Produit.LIBELLE%type;
-	c_PU Produit.PU%type;
-	c_TVA Produit.TVA%type;
+	c_Ref PRODUIT.REF%type;
+	c_Libelle PRODUIT.Libelle%type;
+	c_PU PRODUIT.PU%type;
+	c_TVA PRODUIT.TVA%type;
+	c_CFamille PRODUIT.Code_famille%type;
+	c_CCategorie PRODUIT.Code_categorie%type;
 	CURSOR produits is
-		SELECT PU,TVA FROM Produit WHERE REF='A01'; 
+		SELECT PU,TVA,Libelle FROM Produit WHERE REF='A01'; 
 BEGIN
 	OPEN produits;
 	LOOP
-	FETCH produits into c_PU,c_TVA;
+	FETCH produits into c_PU,c_TVA,c_Libelle;
 		EXIT WHEN produits%notfound;
 		dbms_output.put_line(c_Ref||''||c_Libelle||''||c_PU||''||c_TVA);
 	END LOOP;
